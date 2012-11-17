17822d0427549a583fc18e83ecb2c3480a43fa54
eccentric devotion <eccentricdevotion@home.nz>
2012-11-18 00:13:40 +1300

    Just starting

---
+++ README.md
@@ -0,0 +1,6 @@
+QuickDraw
+===========
+
+A CraftBukkit plugin for Minecraft Server.
+
+QuickDraw lets you make like a wild west gunslinger. Challenge your friends to a snowball fight and see who is the quickest on the draw.
---
+++ src/main/java/me/eccentric_nz/plugins/quickdraw/Quickdraw.java
@@ -0,0 +1,24 @@
+package me.eccentric_nz.plugins.quickdraw;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Quickdraw extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getDisplayName() + "!");
+    }
+}
