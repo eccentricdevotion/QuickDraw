5d93a900b664e2d3976aa332a6aebbe2d32408e3
eccentric devotion <eccentricdevotion@home.nz>
2012-11-18 23:39:11 +1300

    Stop players moving if they are in a challenge

---
+++ src/main/java/me/eccentric_nz/plugins/quickdraw/QuickdrawMoveListener.java
@@ -0,0 +1,21 @@
+package me.eccentric_nz.plugins.quickdraw;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class QuickdrawMoveListener implements Listener {
+
+    private Quickdraw plugin;
+
+    public QuickdrawMoveListener(Quickdraw plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (plugin.invites.containsKey(event.getPlayer().getName()) || plugin.challengers.contains(event.getPlayer().getName())) {
+            event.setTo(event.getFrom());
+        }
+    }
+}
