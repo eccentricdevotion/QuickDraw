4e6540ee89c95767e5f77147367c0a6ea498d050
eccentric devotion <eccentricdevotion@home.nz>
2012-11-20 00:06:11 +1300

    Make sure player can still look around

--- src/main/java/me/eccentric_nz/plugins/quickdraw/QuickdrawMoveListener.java
+++ src/main/java/me/eccentric_nz/plugins/quickdraw/QuickdrawMoveListener.java
@@ -1,5 +1,6 @@
 package me.eccentric_nz.plugins.quickdraw;
 
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -14,8 +15,14 @@ public class QuickdrawMoveListener implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
+        // maybe need plugin.accepted HashMap ?
         if (plugin.invites.containsKey(event.getPlayer().getName()) || plugin.challengers.containsKey(event.getPlayer().getName())) {
-            event.setTo(event.getFrom());
+            if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
+                Location loc = event.getFrom();
+                loc.setPitch(event.getTo().getPitch());
+                loc.setYaw(event.getTo().getYaw());
+                event.getPlayer().teleport(loc);
+            }
         }
     }
 }
