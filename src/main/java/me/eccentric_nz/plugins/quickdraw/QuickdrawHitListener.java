32ebd21733e0d00a2cded1a64402d9f8f5acd5e2
eccentric devotion <eccentricdevotion@home.nz>
2012-11-20 00:05:30 +1300

    If a hit is made record the time, send message and remove from the appropriate hashmap

--- src/main/java/me/eccentric_nz/plugins/quickdraw/QuickdrawHitListener.java
+++ src/main/java/me/eccentric_nz/plugins/quickdraw/QuickdrawHitListener.java
@@ -1,16 +1,21 @@
 package me.eccentric_nz.plugins.quickdraw;
 
-import org.bukkit.entity.Entity;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.Inventory;
 
 public class QuickdrawHitListener implements Listener {
 
     private Quickdraw plugin;
+    QuickdrawDatabase service = QuickdrawDatabase.getInstance();
 
     public QuickdrawHitListener(Quickdraw plugin) {
         this.plugin = plugin;
@@ -25,11 +30,38 @@ public class QuickdrawHitListener implements Listener {
                 if (shooter instanceof Player) {
                     Player hitplayer = (Player) event.getEntity();
                     Player throwplayer = (Player) shooter;
-                    if (plugin.invites.containsKey(hitplayer.getName()) || plugin.challengers.containsKey(hitplayer.getName())) {
-                        plugin.debug(hitplayer.getName() + " was hit by a snowball thrown by " + throwplayer.getName() + "!");
+                    String hNameStr = hitplayer.getName();
+                    String tNameStr = throwplayer.getName();
+                    if (plugin.invites.containsKey(hNameStr) || plugin.challengers.containsKey(hNameStr)) {
+                        plugin.debug(hNameStr + " was hit by a snowball thrown by " + tNameStr + "!");
                         long systime = System.currentTimeMillis();
-                        if (plugin.invites.get(hitplayer.getName()).equalsIgnoreCase(throwplayer.getName()) || plugin.challengers.get(hitplayer.getName()).equalsIgnoreCase(throwplayer.getName())) {
-                            plugin.hittime.put(throwplayer.getName(), systime);
+                        if (tNameStr.equalsIgnoreCase(plugin.invites.get(hNameStr)) || tNameStr.equalsIgnoreCase(plugin.challengers.get(hNameStr))) {
+                            plugin.hittime.put(tNameStr, systime);
+                            long time = plugin.hittime.get(tNameStr) - plugin.drawtime.get(tNameStr);
+                            try {
+                                Connection connection = service.getConnection();
+                                Statement statement = connection.createStatement();
+                                String queryTime = "INSERT INTO quickdraw (player, versus, time) VALUES ('" + tNameStr + "','" + hNameStr + "'," + time + ")";
+                                statement.executeUpdate(queryTime);
+                                ResultSet rsHitInv = statement.executeQuery("SELECT * FROM inventories WHERE player = '" + hNameStr + "'");
+                                if (rsHitInv.next()) {
+                                    String base64 = rsHitInv.getString("inventory");
+                                    Inventory hi = QuickdrawInventory.fromBase64(base64);
+                                    hitplayer.getInventory().setContents(hi.getContents());
+                                    String queryDelete = "DELETE FROM inventories WHERE player = '" + hNameStr + "'";
+                                    statement.executeUpdate(queryDelete);
+                                }
+                                statement.close();
+                            } catch (SQLException e) {
+                                plugin.debug("Could not get players inventory");
+                            }
+                            throwplayer.sendMessage(QuickdrawConstants.MY_PLUGIN_NAME + "Your time against " + hNameStr + " was " + time / 1000 + " seconds");
+                            if (plugin.challengers.containsKey(hNameStr)) {
+                                plugin.challengers.remove(hNameStr);
+                            }
+                            if (plugin.invites.containsKey(hNameStr)) {
+                                plugin.invites.remove(hNameStr);
+                            }
                         }
                     }
                 }
