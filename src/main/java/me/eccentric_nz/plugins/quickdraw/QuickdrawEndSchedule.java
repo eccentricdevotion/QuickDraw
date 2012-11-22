77469a763e524e0fccbe2e6791ec625af363ac60
eccentric devotion <eccentricdevotion@home.nz>
2012-11-23 00:35:41 +1300

    New class to schedulr game end tasks

---
+++ src/main/java/me/eccentric_nz/plugins/quickdraw/QuickdrawEndSchedule.java
@@ -0,0 +1,89 @@
+package me.eccentric_nz.plugins.quickdraw;
+
+import net.milkbowl.vault.economy.EconomyResponse;
+
+public class QuickdrawEndSchedule implements Runnable {
+
+    private Quickdraw plugin;
+    private String pNameStr;
+
+    public QuickdrawEndSchedule(Quickdraw instance, String p) {
+        plugin = instance;
+        pNameStr = p;
+    }
+
+    public void run() {
+        String versus = "", challenger = "";
+        // if one of the palyers missed...
+        if (plugin.challengers.containsKey(pNameStr)) {
+            plugin.debug("The challenger missed...");
+            versus = plugin.challengers.get(pNameStr);
+            challenger = pNameStr;
+            plugin.challengers.remove(pNameStr);
+            plugin.quicktime.put(pNameStr, Long.MAX_VALUE);
+        }
+        if (plugin.accepted.containsKey(pNameStr)) {
+            versus = pNameStr;
+            challenger = plugin.accepted.get(pNameStr);
+            plugin.debug("The invited missed...");
+            plugin.accepted.remove(pNameStr);
+            plugin.quicktime.put(pNameStr, Long.MAX_VALUE);
+        }
+        plugin.debug("Challenger was: " + challenger + ", Versus was: " + versus);
+        // find out who won
+        if (plugin.quicktime.get(versus) > plugin.quicktime.get(challenger)) {
+            // Challenger won
+            plugin.getServer().getPlayer(versus).sendMessage(QuickdrawConstants.MY_PLUGIN_NAME + challenger + " won!");
+            plugin.getServer().getPlayer(challenger).sendMessage(QuickdrawConstants.MY_PLUGIN_NAME + challenger + " won!");
+            if (plugin.purse.containsKey(challenger) && plugin.getServer().getPluginManager().isPluginEnabled("Vault")) {
+                double d = plugin.purse.get(challenger);
+                EconomyResponse deposit = Quickdraw.economy.depositPlayer(challenger, d);
+                Quickdraw.economy.withdrawPlayer(versus, d);
+                if (deposit.transactionSuccess()) {
+                    plugin.getServer().getPlayer(challenger).sendMessage(String.format(QuickdrawConstants.MY_PLUGIN_NAME + "You won %s from the QuickDraw challenge", Quickdraw.economy.format(deposit.amount)));
+                } else {
+                    plugin.getServer().getPlayer(challenger).sendMessage(String.format("An error occured: %s", deposit.errorMessage));
+                }
+            }
+        }
+        if (plugin.quicktime.get(versus) < plugin.quicktime.get(challenger)) {
+            // Versus won
+            plugin.getServer().getPlayer(versus).sendMessage(QuickdrawConstants.MY_PLUGIN_NAME + versus + " won!");
+            plugin.getServer().getPlayer(challenger).sendMessage(QuickdrawConstants.MY_PLUGIN_NAME + versus + " won!");
+            if (plugin.purse.containsKey(challenger) && plugin.getServer().getPluginManager().isPluginEnabled("Vault")) {
+                double d = plugin.purse.get(challenger);
+                EconomyResponse deposit = Quickdraw.economy.depositPlayer(versus, d);
+                Quickdraw.economy.withdrawPlayer(challenger, d);
+                if (deposit.transactionSuccess()) {
+                    plugin.getServer().getPlayer(versus).sendMessage(String.format(QuickdrawConstants.MY_PLUGIN_NAME + "You won %s from the QuickDraw challenge", Quickdraw.economy.format(deposit.amount)));
+                } else {
+                    plugin.getServer().getPlayer(versus).sendMessage(String.format("An error occured: %s", deposit.errorMessage));
+                }
+            }
+        }
+        if (plugin.quicktime.get(versus) == plugin.quicktime.get(challenger)) {
+            // it was a draw
+            plugin.getServer().getPlayer(versus).sendMessage(QuickdrawConstants.MY_PLUGIN_NAME + "It was a draw!");
+            plugin.getServer().getPlayer(challenger).sendMessage(QuickdrawConstants.MY_PLUGIN_NAME + "It was a draw!");
+        }
+        // clear drawtime, hittime and quicktime
+        if (plugin.drawtime.containsKey(versus)) {
+            plugin.drawtime.remove(versus);
+        }
+        if (plugin.drawtime.containsKey(challenger)) {
+            plugin.drawtime.remove(challenger);
+        }
+        if (plugin.hittime.containsKey(versus)) {
+            plugin.hittime.remove(versus);
+        }
+        if (plugin.hittime.containsKey(challenger)) {
+            plugin.hittime.remove(challenger);
+        }
+        if (plugin.quicktime.containsKey(versus)) {
+            plugin.quicktime.remove(versus);
+        }
+        if (plugin.quicktime.containsKey(challenger)) {
+            plugin.quicktime.remove(challenger);
+        }
+    }
+}
