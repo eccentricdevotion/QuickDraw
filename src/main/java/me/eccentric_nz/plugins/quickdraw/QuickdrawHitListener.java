0dd4c6ed1be29c59912f1ecb210ee6b82c494bda
eccentric devotion <eccentricdevotion@home.nz>
2012-11-23 00:37:31 +1300

    Process the throwing player, not the hittee

--- src/main/java/me/eccentric_nz/plugins/quickdraw/QuickdrawHitListener.java
+++ src/main/java/me/eccentric_nz/plugins/quickdraw/QuickdrawHitListener.java
@@ -30,43 +30,35 @@ public class QuickdrawHitListener implements Listener {
                 if (shooter instanceof Player) {
                     Player hitplayer = (Player) event.getEntity();
                     Player throwplayer = (Player) shooter;
-                    String hNameStr = hitplayer.getName();
-                    String tNameStr = throwplayer.getName();
-                    if (plugin.accepted.containsKey(hNameStr) || plugin.challengers.containsKey(hNameStr)) {
-                        plugin.debug(hNameStr + " was hit by a snowball thrown by " + tNameStr + "!");
+                    String hitNameStr = hitplayer.getName();
+                    String throwerNameStr = throwplayer.getName();
+                    if (plugin.drawtime.containsKey(throwerNameStr)) {
+                        plugin.debug(hitNameStr + " was hit by a snowball thrown by " + throwerNameStr + "!");
                         long nanotime = System.nanoTime();
-                        if (tNameStr.equalsIgnoreCase(plugin.accepted.get(hNameStr)) || tNameStr.equalsIgnoreCase(plugin.challengers.get(hNameStr))) {
-                            plugin.hittime.put(tNameStr, nanotime);
-                            long time = plugin.hittime.get(tNameStr) - plugin.drawtime.get(tNameStr);
-                            plugin.quicktime.put(tNameStr, time);
-                            try {
-                                Connection connection = service.getConnection();
-                                Statement statement = connection.createStatement();
-                                String queryTime = "INSERT INTO quickdraw (player, versus, time) VALUES ('" + tNameStr + "','" + hNameStr + "'," + time + ")";
-                                statement.executeUpdate(queryTime);
-                                ResultSet rsHitInv = statement.executeQuery("SELECT * FROM inventories WHERE player = '" + hNameStr + "'");
-                                if (rsHitInv.next()) {
-                                    String base64 = rsHitInv.getString("inventory");
-                                    Inventory hi = QuickdrawInventory.fromBase64(base64);
-                                    hitplayer.getInventory().setContents(hi.getContents());
-                                    String queryDelete = "DELETE FROM inventories WHERE player = '" + hNameStr + "'";
-                                    statement.executeUpdate(queryDelete);
-                                }
-                                statement.close();
-                            } catch (SQLException e) {
-                                plugin.debug("Could not get players inventory");
+                        plugin.hittime.put(throwerNameStr, nanotime);
+                        long time = plugin.hittime.get(throwerNameStr) - plugin.drawtime.get(throwerNameStr);
+                        plugin.quicktime.put(throwerNameStr, time);
+                        try {
+                            Connection connection = service.getConnection();
+                            Statement statement = connection.createStatement();
+                            String queryTime = "INSERT INTO quickdraw (player, versus, time) VALUES ('" + throwerNameStr + "','" + hitNameStr + "'," + time + ")";
+                            statement.executeUpdate(queryTime);
+                            ResultSet rsThrowerInv = statement.executeQuery("SELECT * FROM inventories WHERE player = '" + throwerNameStr + "'");
+                            if (rsThrowerInv.next()) {
+                                String base64 = rsThrowerInv.getString("inventory");
+                                Inventory ti = QuickdrawInventory.fromBase64(base64);
+                                throwplayer.getInventory().setContents(ti.getContents());
+                                String queryDelete = "DELETE FROM inventories WHERE player = '" + throwerNameStr + "'";
+                                statement.executeUpdate(queryDelete);
                             }
-                            double seconds = (double) time / 1000000000.0;
-                            throwplayer.sendMessage(QuickdrawConstants.MY_PLUGIN_NAME + "Your time against " + hNameStr + " was " + seconds + " seconds");
-                            plugin.drawtime.remove(tNameStr);
-                            plugin.hittime.remove(tNameStr);
-                            if (plugin.challengers.containsKey(hNameStr)) {
-                                plugin.challengers.remove(hNameStr);
-                            }
-                            if (plugin.accepted.containsKey(hNameStr)) {
-                                plugin.accepted.remove(hNameStr);
-                            }
+                            statement.close();
+                        } catch (SQLException e) {
+                            plugin.debug("Could not get players inventory");
                         }
+                        double seconds = (double) time / 1000000000.0;
+                        throwplayer.sendMessage(QuickdrawConstants.MY_PLUGIN_NAME + "Your time against " + hitNameStr + " was " + seconds + " seconds");
+                        plugin.drawtime.remove(throwerNameStr);
+                        plugin.hittime.remove(throwerNameStr);
                     }
                 }
             }
